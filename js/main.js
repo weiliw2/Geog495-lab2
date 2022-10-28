1b16108b037a72a39033d99a23bf2c7e11bb1e1d
weiliw2 <weiliw2@uw.edu>
2022-10-28 15:24:23 -0700

    3.24

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ function responsive_control() {
     x.className = "topnav";
   }
 }
+function checkhistory() {
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
